edcb4e18f1bff005ac061eff5ee4a3ede0ee1368
soujash-mandal <2002soujash@gmail.com>
2024-12-02 00:24:32 +0530

    base repo

---
+++ src/routes/index.js
@@ -0,0 +1,9 @@
+const express = require("express");
+
+const router = express.Router();
+
+// Import and use individual routes
+// router.use('/users', require('./userRoutes'));
+// router.use('/products', require('./productRoutes'));
+
+module.exports = router;
